308a488193773b225a460155c0dd1382201772b3
Michaël Hompus <michael@hompus.nl>
2024-11-23 17:25:19 +0100

    Add keywords meta tag and enhance site configuration with author and keywords

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,29 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Michaël’s coding thoughts';
-export const SITE_DESCRIPTION = 'I&#039;m a software developer for Info Support in the Netherlands. I try to blog my experiences on Microsoft technologies I encounter while working on a variety of projects. My specialties are SharePoint, Project Server, LINQ, WCF, Pex, SQL Server, sharing knowledge and presenting.';
+export const SITE_TITLE = "Michaël's coding thoughts";
+export const SITE_DESCRIPTION =
+  "I'm a software developer for Info Support in the Netherlands. I try to blog my experiences on Microsoft technologies I encounter while working on a variety of projects. My specialties are SharePoint, Project Server, LINQ, WCF, Pex, SQL Server, sharing knowledge and presenting.";
+export const SITE_AUTHOR = "Michaël Hompus";
+export const SITE_KEYWORDS = [
+  "blog",
+  "michaël hompus",
+  "software development",
+  "microsoft",
+  "csharp",
+  "c#",
+  "azure",
+  "hyper-v",
+  "microsoft 365",
+  "crm",
+  "sharepoint",
+  "project server",
+  "powershell",
+  "machine learning",
+  "office",
+  "visual studio",
+  "sql",
+  "windows",
+  "speaking",
+];
 export const PAGE_SIZE = 5;
