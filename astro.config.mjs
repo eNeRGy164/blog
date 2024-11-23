3258202acc87bc533c4be89f485249b7335db7b7
Michaël Hompus <michael@hompus.nl>
2024-11-23 16:59:32 +0100

    Enhance Astro configuration; add YAML support, integrate expressive code, and improve rehype plugins for better functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import yaml from "@rollup/plugin-yaml";
+import yamlParser from "yaml";
+import { readFileSync } from "fs";
+import expressiveCode from "astro-expressive-code";
 import remarkGfm from "remark-gfm";
 import rehypeExternalLinks from "rehype-external-links";
-import rehypeCustomImage from "./src/plugins/rehypeCustomImage.js";
 import rehypeAbbreviate from "./src/plugins/rehypeAbbreviate.js";
+import rehypeAddMvpContributorId from "./src/plugins/rehypeAddMvpContributorId.js";
 import rehypeYouTubeEmbed from "./src/plugins/rehypeYouTubeEmbed.js";
-import rehypeAddContributorId from './src/plugins/rehypeAddMvpContributorId.js';
+import rehypeCustomImage from "./src/plugins/rehypeCustomImage.js";
 import { rehypeGithubAlerts } from "rehype-github-alerts";
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://blog.hompus.nl",
   integrations: [sitemap(), expressiveCode({ defaultProps: { wrap: true } })],
@@ -23,9 +24,12 @@ export default defineConfig({
         rehypeExternalLinks,
         {
           content: {},
-          rel: ['noopener', 'noreferrer', 'external'],
+          rel: ["noopener", "noreferrer", "external"],
           target: "_blank",
-          test: (node, _, __) => node.tagName === 'a' && typeof node.properties.href === 'string' && !node.properties.href.includes('linkedin.com'),
+          test: (node, _, __) =>
+            node.tagName === "a" &&
+            typeof node.properties.href === "string" &&
+            !node.properties.href.includes("linkedin.com"),
         },
       ],
       [
@@ -36,12 +40,14 @@ export default defineConfig({
           ).ACRONYMS,
         },
       ],
+      [rehypeAddMvpContributorId, { contributorId: "AZ-MVP-5004268" }],
       rehypeYouTubeEmbed,
       rehypeAccessibleEmojis,
-      rehypeCustomImage
+      rehypeCustomImage,
     ],
+    syntaxHighlight: false,
   },
   vite: {
-    plugins: [yaml()]
-  }
+    plugins: [yaml()],
+  },
 });
