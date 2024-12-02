51310e74987882806ec8eec88ed3d5c681bf3978
Michaël Hompus <michael@hompus.nl>
2024-12-02 23:02:48 +0100

    refactor: simplify urlifyToken function and remove unused stripHTML function

--- src/js/util.js
+++ src/js/util.js
@@ -1,21 +1,11 @@
 'use strict'
 
-// alternative: https://github.com/mdevils/html-entities
-import { decodeHTML } from 'entities'
-
-// if you need a fancier one, you could use https://github.com/sindresorhus/slugify
 export function urlifyToken (s) {
   return s.toLowerCase()
     .replaceAll(' ', '-')
+    .replaceAll('/', '-')
     .replaceAll(':', '')
     .replaceAll('#', 'sharp')
-}
-
-// https://github.com/DylanPiercey/strip
-const htmlReg = /<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi
-const commentReg = /<!--.+-->/gi
-export function stripHTML (html = '') {
-  return decodeHTML(html.replace(htmlReg, '').replace(commentReg, '')).trim()
 }
 
 export function sortedPosts(paths) {
