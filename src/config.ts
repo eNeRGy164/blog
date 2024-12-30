09d27873b827f04abf958208de017162403dbcf9
Michaël Hompus <michael@hompus.nl>
2024-12-30 14:22:41 +0100

    fix: update site title and description

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Michaël's coding thoughts";
+export const SITE_TITLE = "Michaël’s coding thoughts";
 export const SITE_DESCRIPTION =
-  "I'm a software developer for Info Support in the Netherlands. I try to blog my experiences on Microsoft technologies I encounter while working on a variety of projects. My specialties are SharePoint, Project Server, LINQ, WCF, Pex, SQL Server, sharing knowledge and presenting.";
+  "I'm a software architect for Info Support in the Netherlands. I try to blog my experiences working on a diverse set of technologies I encounter while working on a variety of projects. My specialties are dotnet, C#, Azure, Microsoft 365, Java, Dapr, DevOps, sharing knowledge and presenting.";
 export const SITE_AUTHOR = "Michaël Hompus";
 export const SITE_KEYWORDS = [
   "blog",
