695e7957bb0bd6836df4579a45ac17504d7920cf
Michaël Hompus <michael@hompus.nl>
2024-12-30 14:22:51 +0100

    refactor: rename rss.xml.js to feed.xml.js and enhance RSS feed structure

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -5,17 +5,33 @@ import { getCollection } from 'astro:content';
 
 export async function GET (context) {
   const posts = sortedPosts(await getCollection('posts'))
-  
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    customData:
+      '<language>en-us</language>' +
+      `<atom:link href="${context.request.url}" rel="self" type="application/rss+xml" />`,
     items: posts.map((post) => ({
       link: post.data.permalink,
       title: post.data.title,
       pubDate: post.data.date,
-      description: postSummary(post)
-    }))
+      description: postSummary(post),
+      categories: [...post.data.tags, ...post.data.categories],
+      customData:
+        '<language>en-us</language>' +
+        `<dc:creator>${post.data.author}</dc:creator>`
+    })),
+    trailingSlash: false,
+    xmlns: {
+      content: 'http://purl.org/rss/1.0/modules/content/',
+      wfw: 'http://wellformedweb.org/CommentAPI/',
+      dc: 'http://purl.org/dc/elements/1.1/',
+      atom: 'http://www.w3.org/2005/Atom',
+      sy: 'http://purl.org/rss/1.0/modules/syndication/',
+      slash: 'http://purl.org/rss/1.0/modules/slash/',
+    }
   });
 }
 
@@ -31,5 +47,5 @@ function postSummary (post) {
     .join(' ')
     // remove unclosed tags so that the 'Read more' link renders correctly
     .replace(/<\/[a-zA-Z]+$/, '')
-    + ` &hellip; <a href="${post.data.permalink}">Read more</a>`
+    + ` &hellip; <a href="${post.data.permalink.replace(/(\/$)/, "")}">Read more</a>`
 }
